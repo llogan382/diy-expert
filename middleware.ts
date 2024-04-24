8e1f8de682e0d53ef09767490e557512b17f6459
Luke Logan <lukelogan@MacBook-Air.local>
2024-04-24 00:25:49 -0400

    adds notes

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,6 @@ export default clerkMiddleware()
 export const config = {
   matcher: [
     '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
-    '/', // Run middleware on index page
-    '/(api|trpc)(.*)'], // Run middleware on API routes
+    '/'
+   ] // Run middleware on index page
 };
